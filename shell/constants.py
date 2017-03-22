ab56eb56ab6b27169fa5e41240241b6ebccf0f66
an-oreo <an.oreo@live.com>
2017-03-22 10:17:14 -0500

    some consistency imporvements

--- shell/constants.py
+++ shell/constants.py
@@ -4,9 +4,10 @@
 
 
 sample_rate_ = 10000.0
-samples_ = 1000
+samples_ = int(1000)
 min_ = -10.0
 max_ = 10.0
 buf_size_ = int(5000)
+chunk_ = int(10000)
 
 FAKE = True
--- shell/daq_handler.py
+++ shell/daq_handler.py
@@ -4,34 +4,38 @@ import constants as CON
 import os
 from timer import Timer
 
-data = np.zeros(CON.buf_size_, dtype=np.float64)
-time = Timer(0, CON.sample_rate_, CON.buf_size_)
-# time = CON.Time(length=len(data), time_step=1.0/CON.sample_rate_, initial=0.0)
-'''
-for index[i]  time[i] = time[0] + i*time_step
-'''
-# time = np.zeros(CON.buf_size_, dtype=np.float64)
+data = np.zeros(0, dtype=np.float64)
+time = Timer(0, CON.sample_rate_, 0)
 
 
-def buffer_resize(data_size, _dtype=np.float64, maintain=True):
+def buffer_resize(data_size, _dtype=np.float64, maintain=False):
     global data, time
-    if _dtype != data.dtype:
-        data = data.astype(_dtype)
     if maintain:
         buff_size = len(data)
         if buff_size > data_size:
             data = np.concatenate([data, np.zeros(data_size-buff_size, dtype=data.dtype)])
-            # time = np.concatenate([time, np.zeros(data_size-buff_size, dtype=time.dtype)])
+            time = np.concatenate([time, np.zeros(data_size-buff_size, dtype=time.dtype)])
         else:
             data = data[0:data_size]
-            # time = time[0:data_size]
+            time = time[0:data_size]
     else:
         data = np.zeros(data_size, _dtype)
-        # time = np.zeros(data_size, _dtype)
+        time = np.zeros(data_size, _dtype)
 
 
-def fin_read(sample_rate=CON.sample_rate_, samples=CON.samples_, min=CON.min_, max=CON.max_):
-
+def fin_read(samples, sample_rate=CON.sample_rate_,
+             min=CON.min_, max=CON.max_, expand=True, *args, **kwargs):
+    global data, time  # -- may be needed?
+    # buffer_resize(samples)
+    buf_size = len(data)
+    if samples > buf_size:
+        if not expand:
+            samples = buf_size
+            print('Warning: data buffer truncated')
+        else:
+            buffer_resize(samples)
+            # print('extended')
+    from math import ceil
     timeout = ceil(samples * (1.0/sample_rate))
     analog_input = Task()
     read = int32()
@@ -59,12 +63,24 @@ def view(entries: int, tail: bool, *args, **kwargs):
         print(data[0:int(entries)])
 
 
-def save(path, filename, extension):
+def get_path():
+    import os
+    path = './'
+    try:
+        path = os.path.join(os.getcwd(), 'OUTPUT')
+    except FileNotFoundError:
+        os.mkdir('OUTPUT')
+        path = os.path.join(os.getcwd(), 'OUTPUT')
+    return path
+
+
+def save(file_name, path=get_path(), *args, **kwargs):
+    filename, extension = file_name.split('.')
     key = data.dtype.name
     delim_dict = {'.txt': ' ',
                   '.mat': ' ',
                   '.csv': ',',
-                  '.gz' : ' '
+                  '.gz': ' '
                   }
     fmt_dict = {'float64': '%.18e',
                 'float32': '%.9e',
@@ -81,4 +97,3 @@ def save(path, filename, extension):
         for i in range(0, len(data)):
             time_file.write('{:.18e}'.format(temp_time))
             temp_time += time.time_step
-

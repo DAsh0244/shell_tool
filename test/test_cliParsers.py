3814d7e4c35e0743dbb8dc77312b05e8589dad5b
an-oreo <an.oreo@live.com>
2017-05-10 13:11:44 -0500

    added skeleton test file for parsers

---
+++ test/test_cliParsers.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python 
+# vim:fileencoding=utf-8
+# -*- coding: utf-8 -*-
+"""
+shell_tool
+test_cliParsers.py
+Author: Danyal Ahsanullah
+Date: 4/26/2017
+License: N/A
+Description:
+
+"""
+from unittest import TestCase
+
+
+class TestCliParsers(TestCase):
+
+    def test_fin_read_parser(self):
+        raise Exception('Not Yet Implemented -- {}'.format(self.test_fin_read_parser.__name__))
+
+    def test_con_read_parser(self):
+        raise Exception('Not Yet Implemented -- {}'.format(self.test_fin_read_parser.__name__))
+
+    def test_fin_read_parser(self):
+        raise Exception('Not Yet Implemented -- {}'.format(self.test_fin_read_parser.__name__))
+
+    def test_view_parser(self):
+        raise Exception('Not Yet Implemented -- {}'.format(self.test_fin_read_parser.__name__))
+
+    def test_quit_parser(self):
+        raise Exception('Not Yet Implemented -- {}'.format(self.test_fin_read_parser.__name__))
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
+

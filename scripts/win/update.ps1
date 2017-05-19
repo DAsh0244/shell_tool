9ff9a7d8d3b1fd383a39a4b4c55cec4d0ad670f7
an-oreo <an.oreo@live.com>
2017-05-19 18:58:53 -0500

    formatted update script, fixed string comparison method

--- scripts/win/update.ps1
+++ scripts/win/update.ps1
@@ -3,13 +3,16 @@ $startup_path = $((Get-Item -Path ".\\" -Verbose).FullName)
 cd ..\..
 echo "checking update status..."
 git remote update >$null
-if (!($(git status | Select-String $chkstr) -Match $chkstr)){
-    if ($(Read-Host -Prompt "updates available. Update now? (y/n)").trim() -Contains "y") {
-    git pull
+if (!($(git status | Select-String $chkstr) -Match $chkstr))
+{
+    if ($(Read-Host -Prompt "updates available. Update now? (y/n)").trim() -Match "y")
+    {
+        git pull
     }
 }
-else {
-echo "up to date`r`n"
+else
+{
+    echo "up to date`r`n"
 }
 cd $startup_path
 exit
